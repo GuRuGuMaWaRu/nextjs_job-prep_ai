c2e90fff5a7654f6c83893f8019b3c2c8bb283e5
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2025-11-10 20:58:58 +0200

    Set up Arcjet

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,56 @@
+import arcjet, {
+  detectBot,
+  shield,
+  slidingWindow,
+  tokenBucket,
+} from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/"]);
 
+const aj = arcjet({
+  // TODO: add ts for env vars
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    // Shield protects your app from common attacks e.g. SQL injection
+    shield({ mode: "LIVE" }),
+    // Create a bot detection rule
+    detectBot({
+      mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+      // Block all bots except the following
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        // Uncomment to allow these other common bot categories
+        // See the full list at https://arcjet.com/bot-list
+        "CATEGORY:MONITOR", // Uptime monitoring services
+        "CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+      ],
+    }),
+    // Create a token bucket rate limit. Other algorithms are supported.
+    tokenBucket({
+      mode: "LIVE",
+      // Tracked by IP address by default, but this can be customized
+      // See https://docs.arcjet.com/fingerprints
+      //characteristics: ["ip.src"],
+      refillRate: 5, // Refill 5 tokens per interval
+      interval: 10, // Refill every 10 seconds
+      capacity: 10, // Bucket capacity of 10 tokens
+    }),
+    slidingWindow({
+      mode: "LIVE",
+      interval: "1m",
+      max: 100,
+    }),
+  ],
+});
+
 export default clerkMiddleware(async (auth, req) => {
+  const decision = await aj.protect(req, { requested: 5 }); // Deduct 5 tokens from the bucket
+
+  if (decision.isDenied()) {
+    return new Response(null, { status: 403 });
+  }
+
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
