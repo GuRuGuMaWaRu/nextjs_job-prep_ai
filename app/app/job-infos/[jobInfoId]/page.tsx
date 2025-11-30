ac2ddb6b7e8fe91ca0a673fba01c364c9ff543b0
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2025-11-30 16:33:52 +0100

    Add Job Info Details page

---
+++ app/app/job-infos/[jobInfoId]/page.tsx
@@ -0,0 +1,126 @@
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import { ArrowRightIcon } from "lucide-react";
+
+import { BackLink } from "@/core/components/BackLink";
+import { Badge } from "@/core/components/ui/badge";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/core/components/ui/card";
+import { getJobInfo } from "@/core/features/jobInfos/db";
+import { formatExperienceLevel } from "@/core/features/jobInfos/lib/formatters";
+import { getCurrentUser } from "@/core/services/clerk/lib/getCurrentUser";
+import { SuspendedItem } from "@/core/components/SuspendedItem";
+import { Skeleton } from "@/core/components/Skeleton";
+
+const options = [
+  {
+    label: "Answer Technical Questions",
+    description:
+      "Challenge yourself with practice questions tailored to your job description.",
+    href: "questions",
+  },
+  {
+    label: "Practice Interviewing",
+    description: "Simulate a real interview with AI-powered mock interviews.",
+    href: "interviews",
+  },
+  {
+    label: "Refine Your Resume",
+    description:
+      "Get expert feedback on your resume and improve your chances of landing an interview.",
+    href: "resume",
+  },
+  {
+    label: "Update Job Description",
+    description: "This should only be used for minor updates.",
+    href: "edit",
+  },
+];
+
+export default async function JobInfoPage({
+  params,
+}: {
+  params: Promise<{ jobInfoId: string }>;
+}) {
+  const { jobInfoId } = await params;
+
+  const jobInfo = getCurrentUser().then(
+    async ({ userId, redirectToSignIn }) => {
+      if (userId == null) return redirectToSignIn();
+
+      const jobInfo = await getJobInfo(jobInfoId, userId);
+      if (jobInfo == null) return notFound();
+
+      return jobInfo;
+    }
+  );
+
+  return (
+    <div className="container my-4 space-y-4">
+      <BackLink href="/app">Back to Dashboard</BackLink>
+
+      <div className="space-y-6">
+        <header className="space-y-4">
+          <div className="space-y-2">
+            <h1 className="text-3xl md:text-4xl">
+              <SuspendedItem
+                item={jobInfo}
+                fallback={<Skeleton className="w-48" />}
+                result={(j) => j.name}
+              />
+            </h1>
+            <div className="flex gap-2">
+              <SuspendedItem
+                item={jobInfo}
+                fallback={<Skeleton className="w-24" />}
+                result={(j) => (
+                  <Badge variant="secondary">
+                    {formatExperienceLevel(j.experienceLevel)}
+                  </Badge>
+                )}
+              />
+              <SuspendedItem
+                item={jobInfo}
+                fallback={null}
+                result={(j) =>
+                  j.title ? <Badge variant="secondary">{j.title}</Badge> : null
+                }
+              />
+            </div>
+          </div>
+          <p className="text-muted-foreground line-clamp-3">
+            <SuspendedItem
+              item={jobInfo}
+              fallback={<Skeleton className="w-full h-4" />}
+              result={(j) => j.description}
+            />
+          </p>
+        </header>
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 has-hover:*:not-hover:opacity-70">
+          {options.map((option) => (
+            <Link
+              className="hover:scale-[1.02] transition-[transform_opacity]"
+              href={`/app/job-infos/${jobInfoId}/${option.href}`}
+              key={option.href}>
+              <Card className="h-full flex flex-row items-start justify-between">
+                <CardHeader className="grow">
+                  <CardTitle>{option.label}</CardTitle>
+                  <CardDescription>{option.description}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <ArrowRightIcon className="size-6" />
+                </CardContent>
+              </Card>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
