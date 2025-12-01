323ce0e4d543fbce46c329d0928c8ace1768704e
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2025-12-01 11:16:56 +0100

    Modify Clerk styles

--- core/services/clerk/components/ClerkProvider.tsx
+++ core/services/clerk/components/ClerkProvider.tsx
@@ -4,7 +4,32 @@ import { Suspense } from "react";
 export function ClerkProvider({ children }: { children: React.ReactNode }) {
   return (
     <Suspense>
-      <OriginalClerkProvider>{children}</OriginalClerkProvider>
+      <OriginalClerkProvider
+        appearance={{
+          cssLayerName: "vendor",
+          variables: {
+            colorBackground: "var(--color-background)",
+            borderRadius: "var(--radius-md)",
+            colorBorder: "var(--color-secondary-foreground)",
+            colorDanger: "var(--color-destructive)",
+            colorForeground: "var(--color-foreground)",
+            colorPrimary: "var(--color-primary)",
+            colorPrimaryForeground: "var(--color-primary-foreground)",
+            colorInput: "var(--color-input)",
+            colorInputForeground: "var(--color-text)",
+            colorMuted: "var(--color-muted)",
+            colorMutedForeground: "var(--color-muted-foreground)",
+            colorNeutral: "var(--color-secondary-foreground)",
+            colorRing: "var(--color-ring)",
+            colorShadow: "var(--color-shadow-color)",
+            colorSuccess: "var(--color-primary)",
+            colorWarning: "var(--color-warning)",
+            fontFamily: "var(--font-sans)",
+            fontFamilyButtons: "var(--font-sans)",
+          },
+        }}>
+        {children}
+      </OriginalClerkProvider>
     </Suspense>
   );
 }
