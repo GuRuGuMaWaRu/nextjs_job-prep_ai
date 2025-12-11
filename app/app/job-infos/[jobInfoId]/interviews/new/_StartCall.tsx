5dfbfce0a3e089acffeb92b1cfd8480d7969cea8
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2025-12-11 09:08:53 +0100

    Rework UX for New Interview page

--- app/app/job-infos/[jobInfoId]/interviews/new/_StartCall.tsx
+++ app/app/job-infos/[jobInfoId]/interviews/new/_StartCall.tsx
@@ -1,19 +1,28 @@
 "use client";
 
 import { useMemo, useState, useEffect, useRef } from "react";
+import { useRouter } from "next/navigation";
 import { useVoice, VoiceReadyState } from "@humeai/voice-react";
-import { Loader2Icon, MicIcon, MicOffIcon, PhoneOffIcon } from "lucide-react";
+import {
+  Loader2Icon,
+  MicIcon,
+  MicOffIcon,
+  PhoneCallIcon,
+  PhoneOffIcon,
+} from "lucide-react";
 
 import { env } from "@/core/data/env/client";
 import { Button } from "@/core/components/ui/button";
+import { BackLink } from "@/core/components/BackLink";
 import { JobInfoTable } from "@/core/drizzle/schema";
 import { CondensedMessages } from "@/core/services/hume/components/CondensedMessages";
 import { condenseChatMessages } from "@/core/services/hume/lib/condenseChatMessages";
-import { createInterview } from "@/core/features/interviews/actions";
-import { errorToast } from "@/core/lib/errorToast";
-import { updateInterview } from "@/core/features/interviews/actions";
-import { useRouter } from "next/navigation";
-import { BackLink } from "@/core/components/BackLink";
+import {
+  createInterview,
+  updateInterview,
+} from "@/core/features/interviews/actions";
+import { errorToast, HUME_UNAVAILABLE_MESSAGE } from "@/core/lib/errorToast";
+import { cn } from "@/core/lib/utils";
 
 export function StartCall({
   accessToken,
@@ -29,8 +38,14 @@ export function StartCall({
 }) {
   const router = useRouter();
 
-  const { connect, readyState, chatMetadata, callDurationTimestamp } =
-    useVoice();
+  const {
+    connect,
+    disconnect,
+    readyState,
+    messages,
+    chatMetadata,
+    callDurationTimestamp,
+  } = useVoice();
   const [interviewId, setInterviewId] = useState<string | null>(null);
   const durationRef = useRef(callDurationTimestamp);
 
@@ -62,9 +77,41 @@ export function StartCall({
     durationRef.current = callDurationTimestamp;
   }, [callDurationTimestamp]);
 
-  //** Handle disconnect */
+  //** This means Hume prevents us from having an interview, so we need to show a toast */
   useEffect(() => {
-    if (readyState !== VoiceReadyState.CLOSED) return;
+    if (readyState === VoiceReadyState.CLOSED && !messages.length) {
+      errorToast(HUME_UNAVAILABLE_MESSAGE);
+    }
+  }, [jobInfo.id, messages.length, readyState, router]);
+
+  const handleStartInterview = async () => {
+    const res = await createInterview({ jobInfoId: jobInfo.id });
+
+    if (res.error) {
+      errorToast(res.message);
+      return;
+    }
+
+    setInterviewId(res.id);
+
+    connect({
+      auth: { type: "accessToken", value: accessToken },
+      configId: env.NEXT_PUBLIC_HUME_CONFIG_ID,
+      sessionSettings: {
+        type: "session_settings",
+        variables: {
+          userName: user.name,
+          experienceLevel: jobInfo.experienceLevel,
+          title: jobInfo.title || "Not specified",
+          description: jobInfo.description,
+        },
+      },
+    });
+  };
+
+  const handleEndInterview = () => {
+    disconnect();
+
     if (interviewId == null) {
       return router.push(`/app/job-infos/${jobInfo.id}/interviews`);
     }
@@ -72,66 +119,29 @@ export function StartCall({
     if (durationRef.current != null) {
       updateInterview(interviewId, { duration: durationRef.current });
     }
-
     router.push(`/app/job-infos/${jobInfo.id}/interviews/${interviewId}`);
-  }, [interviewId, jobInfo.id, readyState, router]);
-
-  if (readyState === VoiceReadyState.IDLE) {
-    return (
-      <div className="h-screen-header container py-4 flex flex-col">
-        <BackLink
-          href={`/app/job-infos/${jobInfo.id}/interviews`}
-          className="self-start">
-          Back To Interviews
-        </BackLink>
-        <Button
-          className="w-fit self-center my-auto"
-          size="lg"
-          onClick={async () => {
-            const res = await createInterview({ jobInfoId: jobInfo.id });
-
-            if (res.error) {
-              return errorToast(res.message);
-            }
-
-            setInterviewId(res.id);
-
-            connect({
-              auth: { type: "accessToken", value: accessToken },
-              configId: env.NEXT_PUBLIC_HUME_CONFIG_ID,
-              sessionSettings: {
-                type: "session_settings",
-                variables: {
-                  userName: user.name,
-                  experienceLevel: jobInfo.experienceLevel,
-                  title: jobInfo.title || "Not specified",
-                  description: jobInfo.description,
-                },
-              },
-            });
-          }}>
-          Start Interview
-        </Button>
-      </div>
-    );
-  }
-
-  if (
-    readyState === VoiceReadyState.CONNECTING ||
-    readyState === VoiceReadyState.CLOSED
-  ) {
-    return (
-      <div className="h-screen-header flex items-center justify-center">
-        <Loader2Icon className="animate-spin size-24" />
-      </div>
-    );
-  }
+  };
 
   return (
-    <div className="overflow-y-auto h-screen-header flex flex-col-reverse">
-      <div className="container py-6 flex flex-col items-center justify-end gap-4">
-        <Messages user={user} />
-        <Controls />
+    <div className="container h-screen-header flex flex-col justify-between py-4">
+      <BackLink
+        href={`/app/job-infos/${jobInfo.id}/interviews`}
+        className="self-start">
+        Back to Interviews
+      </BackLink>
+
+      {readyState === VoiceReadyState.CONNECTING ? (
+        <Loader2Icon className="animate-spin size-24 self-center" />
+      ) : null}
+
+      <div className="overflow-y-auto flex flex-col-reverse">
+        <div className="container py-6 flex flex-col items-center justify-end gap-4">
+          <Messages user={user} />
+          <Controls
+            onEndInterview={handleEndInterview}
+            onStartInterview={handleStartInterview}
+          />
+        </div>
       </div>
     </div>
   );
@@ -154,17 +164,50 @@ function Messages({ user }: { user: { name: string; image: string } }) {
   );
 }
 
-function Controls() {
-  const { disconnect, isMuted, mute, unmute, micFft, callDurationTimestamp } =
+function Controls({
+  onEndInterview,
+  onStartInterview,
+}: {
+  onEndInterview: () => void;
+  onStartInterview: () => void;
+}) {
+  const { isMuted, mute, unmute, micFft, callDurationTimestamp, readyState } =
     useVoice();
+
+  const beforeInterview = readyState === VoiceReadyState.IDLE;
+  const interviewIsActive = readyState === VoiceReadyState.OPEN;
 
   return (
     <div className="flex gap-5 rounded border px-4 py-2 w-fit sticky bottom-6 bg-background items-center">
       <Button
         variant="ghost"
         size="icon"
-        className="-mx-3"
-        onClick={() => (isMuted ? unmute() : mute())}>
+        className={cn(
+          "-mx-3",
+          !beforeInterview && "opacity-50 cursor-not-allowed"
+        )}
+        onClick={() => {
+          if (!beforeInterview) return;
+          onStartInterview();
+        }}>
+        <PhoneCallIcon className="size-4 text-primary" />
+        <span className="sr-only">Start Interview</span>
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        className={cn(
+          "-mx-3",
+          !interviewIsActive && "opacity-50 cursor-not-allowed"
+        )}
+        onClick={() => {
+          if (!interviewIsActive) return;
+          if (isMuted) {
+            unmute();
+          } else {
+            mute();
+          }
+        }}>
         {isMuted ? <MicOffIcon className="text-destructive" /> : <MicIcon />}
         <span className="sr-only">{isMuted ? "Unmute" : "Mute"}</span>
       </Button>
@@ -177,8 +220,14 @@ function Controls() {
       <Button
         variant="ghost"
         size="icon"
-        className="-mx-3"
-        onClick={disconnect}>
+        className={cn(
+          "-mx-3",
+          !interviewIsActive && "opacity-50 cursor-not-allowed"
+        )}
+        onClick={() => {
+          if (!interviewIsActive) return;
+          onEndInterview();
+        }}>
         <PhoneOffIcon className="text-destructive" />
         <span className="sr-only">End Call</span>
       </Button>
