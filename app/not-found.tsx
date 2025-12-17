a8f3052c8037f73c59deb708cec16286b8cb4cc3
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2025-12-17 12:31:26 +0100

    feat: add a top layer not-found page with redirect button

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+import { Button } from "@/core/components/ui/button";
+
+export default function NotFound() {
+  return (
+    <div className="container max-w-5xl flex flex-col items-center justify-center h-screen-header gap-2">
+      <p className="text-primary text-2xl font-bold">
+        Oopsie! No such page found.
+      </p>
+      <Button asChild className="mt-2">
+        <Link href="/" replace>
+          Go to Home Page
+        </Link>
+      </Button>
+    </div>
+  );
+}
